08f03e123dd06791f1afa2ff30cb22d1af59b0d1
Teusner <quentin.brateau@ensta-bretagne.org>
2023-09-07 11:15:03 +0200

    Fixing comments

--- src/immersion_controller.cpp
+++ src/immersion_controller.cpp
@@ -166,14 +166,14 @@ namespace riptide_controllers {
                 }
 
                 else if (time > immersion_start_time_ + *phase_1_duration_) {
-                    // Going upwards
+                    // Going downwards
                     command_interfaces_[0].set_value(params_.thruster_velocity);
                     command_interfaces_[1].set_value(0.);
                     command_interfaces_[2].set_value(params_.fin_angle);
                     command_interfaces_[3].set_value(-params_.fin_angle);
                 }
                 else {
-                    // Going downwards
+                    // Going upwards
                     command_interfaces_[0].set_value(params_.thruster_velocity);
                     command_interfaces_[1].set_value(0.);
                     command_interfaces_[2].set_value(-params_.fin_angle);
